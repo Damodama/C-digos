cfe077b5ba48636334097541f354f13848ddd2c6
Damodama <sarah.elainne@aluno.fiema.org.br>
2025-10-16 11:13:14 -0300

    Add files via upload

---
+++ L1, ling. C++/retangulo.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+    double base,altura,area,perimetro,diagonal;
+    cout << "Digite a medida da base do retangulo: ";
+    cin >> base;
+    cout << "Digite a medida da altura do retangulo: ";
+    cin >> altura;
+    area=base*altura;
+    perimetro=2*(base+altura);
+    diagonal = sqrt(base*base+altura*altura);
+    cout << fixed << setprecision(4);
+    cout << "AREA = " << area << endl;
+    cout << "PERIMETRO = " << perimetro << endl;
+    cout << "DIAGONAL = " << diagonal << endl;
+    return 0;
+}
